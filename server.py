0359125bc2e038a57ed36db2d09ae732c99f79ee
Adam Jenca <jenca.adam@gmail.com>
2025-10-18 18:28:47 +0200

    i might be stupid :)

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ import gt_api
 from gt_api.errors import GeotasticAPIError
 import random
 DROP = {'id': 7726457, 'owner': 1573548, 'style': 'streetview', 'lat': 48.4403605385592, 'lng': 19.796818194444, 'mapId': 14155, 'challengeId': 0, 'groupId': 0, 'code': 'sk', 'subCode': 'sk-bc', 'wikiId': 0, 'panoId': 'QsvGtGRPk1h_xzMrW1kJIw', 'heading': 0, 'pitch': 0, 'zoom': 0, 'disallowTrekker': -1, 'imageUrl': None, 'copyright': None, 'hint': None, 'photosphereData': None, 'hideCompass': False, 'isLandmarkTip': False, 'sorting': 28, 'radius': 0, 'type': 'single'} # for now
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/fetch_drop", methods = ["GET"])
-def fetch_drop(url):
+def fetch_drop():
     if "token" not in request.args:
         return {"status":"error", "message":"no token", "data":{}}, 401
     try:
@@ -14,3 +14,7 @@ def fetch_drop(url):
     except GeotasticAPIError as e:
         return {"status":"error", "message":"invalid token", "data":{}}, 403
     return {"status":"ok", "message":"", "data":{"drop":DROP}}
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
