33cfe9af9f62fc74bbd8db0f8e51446c1062aef0
root <root@gtedit.tech>
2025-10-18 22:13:45 +0200

    cors

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ import gt_api
 from gt_api.errors import GeotasticAPIError
 import random
 import database
-
+from flask_cors import CORS
 DROP = {'id': 7726457, 'owner': 1573548, 'style': 'streetview', 'lat': 48.4403605385592, 'lng': 19.796818194444, 'mapId': 14155, 'challengeId': 0, 'groupId': 0, 'code': 'sk', 'subCode': 'sk-bc', 'wikiId': 0, 'panoId': 'QsvGtGRPk1h_xzMrW1kJIw', 'heading': 0, 'pitch': 0, 'zoom': 0, 'disallowTrekker': -1, 'imageUrl': None, 'copyright': None, 'hint': None, 'photosphereData': None, 'hideCompass': False, 'isLandmarkTip': False, 'sorting': 28, 'radius': 0, 'type': 'single'}, # for now
 DROP_INFO = {'title':'Hello, world!', 'description':"if you see this, the server works", 'link':'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'image':"https://http.cat/418"}
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/fetch_drop", methods = ["GET"])
 def fetch_drop():
     if "token" not in request.args:
