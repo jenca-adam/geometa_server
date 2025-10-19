8d144276247c795692b5fb7e25f769b02d5b5305
Adam Jenca <jenca.adam@gmail.com>
2025-10-19 09:32:31 +0200

    to_json() fixed

--- database.py
+++ database.py
@@ -5,8 +5,11 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
+
+
 class Base(DeclarativeBase):
     pass
+
 
 meta_tag_association = Table(
     "meta_tag_association",
@@ -18,53 +21,73 @@ meta_tag_association = Table(
 
 class Tag(Base):
     __tablename__ = "tag"
-    id:Mapped[int] = mapped_column(primary_key = True)
-    name:Mapped[str] = mapped_column(String(128)) 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(128))
     parent_id: Mapped[int] = mapped_column(ForeignKey("tag.id"), nullable=True)
-    children: Mapped[list["Tag"]] = relationship(back_populates = "parent",  cascade="all")
-    parent: Mapped["Tag"] = relationship(remote_side=[id], back_populates = "children")
-    metas: Mapped[list["Meta"]] = relationship(secondary = meta_tag_association, back_populates="tags")
+    children: Mapped[list["Tag"]] = relationship(back_populates="parent", cascade="all")
+    parent: Mapped["Tag"] = relationship(remote_side=[id], back_populates="children")
+    metas: Mapped[list["Meta"]] = relationship(
+        secondary=meta_tag_association, back_populates="tags"
+    )
+
 
 class Meta(Base):
     __tablename__ = "meta"
-    id:Mapped[int] = mapped_column(primary_key = True)
-    meta_data:Mapped[dict] = mapped_column(JSON) # Make the metadata format flexible, so that changing it doesn't require redoing the entire database.
-    gt_user_uid:Mapped[str] = mapped_column(String(10))
-    tags:Mapped[list["Tag"]] = relationship(secondary = meta_tag_association, back_populates="metas")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    meta_data: Mapped[dict] = mapped_column(
+        JSON
+    )  # Make the metadata format flexible, so that changing it doesn't require redoing the entire database.
+    gt_user_uid: Mapped[str] = mapped_column(String(10))
+    tags: Mapped[list["Tag"]] = relationship(
+        secondary=meta_tag_association, back_populates="metas"
+    )
     drops: Mapped[list["Drop"]] = relationship(back_populates="meta")
-    
+
+    def to_json(self):
+        return {**self.meta_data, "tags": [tag.name for tag in self.tags]}
+
+
 class Drop(Base):
     __tablename__ = "drop"
-    id:Mapped[int] = mapped_column(primary_key = True)
-    drop_data:Mapped[dict] = mapped_column(JSON)
-    meta_id:Mapped[int] = mapped_column(ForeignKey("meta.id"))
-    meta:Mapped["Meta"] = relationship(back_populates = "drops")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    drop_data: Mapped[dict] = mapped_column(JSON)
+    meta_id: Mapped[int] = mapped_column(ForeignKey("meta.id"))
+    meta: Mapped["Meta"] = relationship(back_populates="drops")
+
     def to_json(self):
-        return {"drop":self.drop_data, "dropInfo":self.meta_data, "tags":[tag.name for tag in self.tags]}
+        return {"drop": self.drop_data, "dropInfo": self.meta.to_json()}
+
 
 engine = create_engine("sqlite:///db.sqlite")
 Base.metadata.create_all(engine)
-Session = sessionmaker(bind = engine)
+Session = sessionmaker(bind=engine)
 session = Session()
+
 
 def pick_random_drop():
     return session.query(Drop).order_by(func.random()).first()
 
+
 def create_drop(drop_data, meta):
-    drop = Drop(drop_data= drop_data, meta=meta)
+    drop = Drop(drop_data=drop_data, meta=meta)
     session.add(drop)
     session.commit()
     return drop
+
+
 def create_meta(meta_data, gt_user_uid, tags):
-    meta = Meta(meta_data = meta_data, gt_user_uid=gt_user_uid, tags=tags)
+    meta = Meta(meta_data=meta_data, gt_user_uid=gt_user_uid, tags=tags)
     session.add(meta)
     session.commit()
     return meta
+
+
 def create_tag(name, parent=None):
     tag = Tag(name=name, parent=parent)
     session.add(tag)
     session.commit()
     return tag
-def get_tag(name):
-    return session.query(Tag).filter(Tag.name==name).first()
 
+
+def get_tag(name):
+    return session.query(Tag).filter(Tag.name == name).first()
--- server.py
+++ server.py
@@ -4,25 +4,65 @@ from gt_api.errors import GeotasticAPIError
 import random
 import database
 from flask_cors import CORS
-DROP = {'id': 7726457, 'owner': 1573548, 'style': 'streetview', 'lat': 48.4403605385592, 'lng': 19.796818194444, 'mapId': 14155, 'challengeId': 0, 'groupId': 0, 'code': 'sk', 'subCode': 'sk-bc', 'wikiId': 0, 'panoId': 'QsvGtGRPk1h_xzMrW1kJIw', 'heading': 0, 'pitch': 0, 'zoom': 0, 'disallowTrekker': -1, 'imageUrl': None, 'copyright': None, 'hint': None, 'photosphereData': None, 'hideCompass': False, 'isLandmarkTip': False, 'sorting': 28, 'radius': 0, 'type': 'single'}, # for now
-DROP_INFO = {'title':'Hello, world!', 'description':"if you see this, the server works", 'link':'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'image':"https://http.cat/418"}
+
+DROP = (
+    {
+        "id": 7726457,
+        "owner": 1573548,
+        "style": "streetview",
+        "lat": 48.4403605385592,
+        "lng": 19.796818194444,
+        "mapId": 14155,
+        "challengeId": 0,
+        "groupId": 0,
+        "code": "sk",
+        "subCode": "sk-bc",
+        "wikiId": 0,
+        "panoId": "QsvGtGRPk1h_xzMrW1kJIw",
+        "heading": 0,
+        "pitch": 0,
+        "zoom": 0,
+        "disallowTrekker": -1,
+        "imageUrl": None,
+        "copyright": None,
+        "hint": None,
+        "photosphereData": None,
+        "hideCompass": False,
+        "isLandmarkTip": False,
+        "sorting": 28,
+        "radius": 0,
+        "type": "single",
+    },
+)  # for now
+DROP_INFO = {
+    "title": "Hello, world!",
+    "description": "if you see this, the server works",
+    "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "image": "https://http.cat/418",
+}
 app = Flask(__name__)
 CORS(app)
-@app.route("/fetch_drop", methods = ["GET"])
+
+
+@app.route("/fetch_drop", methods=["GET"])
 def fetch_drop():
     """if "token" not in request.args:
         return {"status":"error", "message":"no token", "data":{}}, 401
     try:
-        gt_api.user.get_user_info(request.args['token']) 
+        gt_api.user.get_user_info(request.args['token'])
     except GeotasticAPIError as e:
         return {"status":"error", "message":"invalid token", "data":{}}, 403"""
     drop = database.pick_random_drop()
     if drop is None:
-        return {"status":"error", "message":"no drops in database", "data":{}}, 400
+        return {"status": "error", "message": "no drops in database", "data": {}}, 400
 
-    return {"status":"ok", "message":"", "data":drop.to_json()}
-@app.route("/get_tags", methods = ["GET"])
+    return {"status": "ok", "message": "", "data": drop.to_json()}
+
+
+@app.route("/get_tags", methods=["GET"])
 def get_tags():
     return [tag.name for tag in database.session.query(database.Tag).all()]
+
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
